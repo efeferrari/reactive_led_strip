e910055aedf7e44328f4aed23b5f60ed2bf9e3ac
Franco Ferrari <franco.ferrari@giantmonkeyrobot.com>
2020-11-16 11:28:33 -0300

    re indent

--- ledStripControl.ino
+++ ledStripControl.ino
@@ -3,7 +3,7 @@
 /** Hardware configuration **/
 #define LED_PIN 6
 #define ANALOG_READ 0
-#define LED_TYPE NEOPIXEL /* antes decía NEOPIXEL WS2812B */
+#define LED_TYPE NEOPIXEL /* NEOPIXEL WS2812B */
 
 /** Environtment params **/
 #define NUM_LEDS 10
@@ -13,41 +13,41 @@
 /** Global configs **/
 CRGB leds[NUM_LEDS];
 
-void setup() {
-  
-  Serial.begin(9600);
+void setup()
+{
+    Serial.begin(9600);
 
-  // turn on every led to check if all it's working well
-  FastLED.addLeds<LED_TYPE, LED_PIN>(leds, NUM_LEDS);
+    // turn on every led to check if all it's working well
+    FastLED.addLeds<LED_TYPE, LED_PIN>(leds, NUM_LEDS);
 
-  clearStrip();
-  delay(250);
-  initLights();
+    clearStrip();
+    delay(250);
+    initLights();
 }
 
 void loop() {
-  rainbow();
+    rainbow();
 }
 
 void rainbow()
 {
-  for (int j = 0; j < 255; j++) {
-    for (int i = 0; i < NUM_LEDS; i++) {
-      leds[i] = CHSV(i - (j * 2), SATURATION, BRIGHTNESS); /* The higher the value 4 the less fade there is and vice versa */ 
+    for (int j = 0; j < 255; j++) {
+        for (int i = 0; i < NUM_LEDS; i++) {
+            leds[i] = CHSV(i - (j * 2), SATURATION, BRIGHTNESS); /* The higher the value 4 the less fade there is and vice versa */
+        }
+        FastLED.show();
+        delay(30); /* Change this to your hearts desire, the lower the value the faster your colors move (and vice versa) */
     }
-    FastLED.show();
-    delay(30); /* Change this to your hearts desire, the lower the value the faster your colors move (and vice versa) */
-  }
 }
 
 void initLights()
 {
-  for(int i=0; i<NUM_LEDS; i++)
-  {
-    leds[i] = CRGB(randomColor(), randomColor(), randomColor());
-    FastLED.show();
-    delay(80);
-  }
+    for(int i=0; i<NUM_LEDS; i++)
+    {
+        leds[i] = CRGB(randomColor(), randomColor(), randomColor());
+        FastLED.show();
+        delay(80);
+    }
 }
 
 /**
@@ -55,11 +55,12 @@ void initLights()
  */
 void clearStrip()
 {
-  for(int i=0; i<NUM_LEDS; i++)
-  {
-    leds[i] = CRGB(0, 0, 0);
-  }
-  FastLED.show();
+    for(int i=0; i<NUM_LEDS; i++)
+    {
+        leds[i] = CRGB(0, 0, 0);
+    }
+
+    FastLED.show();
 }
 
 /**
@@ -67,5 +68,14 @@ void clearStrip()
  */
 int randomColor()
 {
-  return random(0, 255);
+    return random(0, 255);
 }
+
+
+
+
+
+
+
+
+
